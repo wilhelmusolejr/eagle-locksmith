e04e57d645ba87597cb079c53c3e8e02f4dab50b
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-04-02 20:45:18 +0800

    fix bug. basic initialized

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
   return (
     <>
-      <div>test</div>
+      <header>
+        {/* mini header */}
+        <div className="mini-header p-5">
+          <div className="text-center ">
+            <div className="flex items-center justify-center gap-2 pb-5">
+              <FontAwesomeIcon icon={faPhone} className="text-orange-500 w-5" />
+              <p className="">+1 (844) 411-5625</p>
+            </div>
+
+            <p className="uppercase font-semibold blue">
+              We Provide fast, reliable and affordable locksmith services
+            </p>
+          </div>
+        </div>
+
+        {/* navigator */}
+      </header>
     </>
   );
 }
