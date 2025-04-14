145b2852f5d27180540959a64f141bccc7e42c2a
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-04-14 19:28:30 +0800

    add navbar for mobile device

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -2,12 +2,19 @@
 
 import Logo from "./Logo";
 import Icon from "./Icon";
-import { faAngleDown, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faBars,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigator() {
   const pathname = usePathname();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navLinks = [
     { name: "Home", path: "/" },
@@ -18,14 +25,18 @@ export default function Navigator() {
     { name: "Contact us", path: "/contact" },
   ];
 
+  const handleToggleClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="z-20 navigator text-white shadow-lg sticky top-0">
+    <nav className="sticky top-0 z-20 text-white shadow-lg navigator">
       <div className="flex items-center justify-between px-5 xl:px-10 py-7 ">
         <div className="flex gap-10">
           {/* LOGO */}
           <Logo size={70} className="w-fit" />
 
-          <ul className="capitalize font-light items-center gap-5 hidden lg:flex text-xl">
+          <ul className="items-center hidden gap-5 text-xl font-light capitalize lg:flex">
             <li
               className={`${
                 pathname === "/" ? "font-bold" : ""
@@ -45,9 +56,9 @@ export default function Navigator() {
             <li className="relative group">
               <button className="focus:outline-none">
                 Services
-                <Icon icon={faAngleDown} className="w-4 inline-block ml-2" />
+                <Icon icon={faAngleDown} className="inline-block w-4 ml-2" />
               </button>
-              <ul className="absolute top-full left-0 w-40 bg-white text-black shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:visible invisible transition-all duration-300 z-50">
+              <ul className="absolute left-0 z-50 invisible w-40 text-black transition-all duration-300 bg-white rounded-md shadow-lg opacity-0 top-full group-hover:opacity-100 group-hover:visible">
                 <li className="px-4 py-2 text-lg hover:bg-gray-100">
                   <Link href="/services/commercial-locksmith">
                     Commercial Locksmith
@@ -98,17 +109,130 @@ export default function Navigator() {
           </ul>
         </div>
 
-        <div className="flex items-center flex-row gap-5">
+        <div className="flex flex-row items-center gap-5">
           {/* CTA */}
           <div className="cta">
-            <button className="p-3 md:text-2xl shadow-2xl cursor-pointer font-bold uppercase bg-red-700 rounded-md btn btn-primary hover:bg-red-800 hover:shadow-xl transition-all duration-300">
+            <button className="p-3 font-bold uppercase transition-all duration-300 bg-red-700 rounded-md shadow-2xl cursor-pointer md:text-2xl btn btn-primary hover:bg-red-800 hover:shadow-xl">
               Get A Quote
             </button>
           </div>
 
           {/* BAR */}
-          <Icon icon={faBars} className="w-7 custom-nav-toggle" />
+          <button
+            onClick={handleToggleClick}
+            className="lg:hidden"
+            aria-label="Toggle menu"
+          >
+            <Icon icon={faBars} className="w-7 custom-nav-toggle" />
+          </button>
         </div>
+
+        {/* Example: Conditional menu */}
+        {isMenuOpen && (
+          <nav className="fixed inset-0 z-20 text-white bg-blue-800">
+            <div className="flex flex-col items-center justify-center h-screen px-5 gap-15 xl:px-10 py-7">
+              <div className="flex flex-col gap-10">
+                {/* LOGO */}
+                <Logo size={70} className="mx-auto w-fit" />
+
+                <ul className="flex flex-col items-center gap-5 text-xl font-light capitalize ">
+                  <li
+                    className={`${
+                      pathname === "/" ? "font-bold" : ""
+                    } hover:underline transition-all duration-300`}
+                  >
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li
+                    className={`${
+                      pathname === "/about" ? "font-bold" : ""
+                    } hover:underline transition-all duration-300`}
+                  >
+                    <Link href="/about">About us</Link>
+                  </li>
+
+                  {/* drop down */}
+                  <li className="relative group">
+                    <button className="focus:outline-none">
+                      Services
+                      <Icon
+                        icon={faAngleDown}
+                        className="inline-block w-4 ml-2"
+                      />
+                    </button>
+                    <ul className="absolute left-0 z-50 invisible w-40 text-black transition-all duration-300 bg-white rounded-md shadow-lg opacity-0 top-full group-hover:opacity-100 group-hover:visible">
+                      <li className="px-4 py-2 text-lg hover:bg-gray-100">
+                        <Link href="/services/commercial-locksmith">
+                          Commercial Locksmith
+                        </Link>
+                      </li>
+                      <li className="px-4 py-2 text-lg hover:bg-gray-100">
+                        <Link href="/services/residential-locksmith">
+                          Residential Locksmith
+                        </Link>
+                      </li>
+                      <li className="px-4 py-2 text-lg hover:bg-gray-100">
+                        <Link href="/services/emergency-locksmith">
+                          Emergency Locksmith
+                        </Link>
+                      </li>
+                      <li className="px-4 py-2 text-lg hover:bg-gray-100">
+                        <Link href="/services/automotive-locksmith">
+                          Automotive Locksmith
+                        </Link>
+                      </li>
+                      <li className="px-4 py-2 text-lg hover:bg-gray-100">
+                        <Link href="/services/safe-locksmith">
+                          Safe Locksmith
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li
+                    className={`${
+                      pathname === "/locations" ? "font-bold" : ""
+                    } hover:underline transition-all duration-300`}
+                  >
+                    <Link href="/locations">Locations</Link>
+                  </li>
+                  <li
+                    className={`${
+                      pathname === "/blog" ? "font-bold" : ""
+                    } hover:underline transition-all duration-300`}
+                  >
+                    <a href="/blog">Blog</a>
+                  </li>
+                  <li
+                    className={`${
+                      pathname === "/contact" ? "font-bold" : ""
+                    } hover:underline transition-all duration-300`}
+                  >
+                    <Link href="/contact">Contact us</Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="flex flex-row items-center gap-5 ">
+                {/* CTA */}
+                <div className="cta">
+                  <button className="p-3 font-bold uppercase transition-all duration-300 bg-red-700 rounded-md shadow-2xl cursor-pointer md:text-2xl btn btn-primary hover:bg-red-800 hover:shadow-xl">
+                    Get A Quote
+                  </button>
+                </div>
+
+                {/* BAR */}
+                <button
+                  onClick={handleToggleClick}
+                  className="absolute lg:hidden top-10 right-10"
+                  aria-label="Toggle menu"
+                >
+                  <Icon icon={faClose} className="w-7 custom-nav-toggle" />
+                </button>
+              </div>
+            </div>
+          </nav>
+        )}
       </div>
     </nav>
   );
