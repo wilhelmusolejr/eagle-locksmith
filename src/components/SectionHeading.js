baa14529916b9b60e9674c20796b62191edabbe4
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-04-16 12:32:24 +0800

    #

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function SectionHeading({ children, type = "big" }) {
   if (type === "big") {
     return <h2 className="text-5xl font-bold uppercase ">{children}</h2>;
