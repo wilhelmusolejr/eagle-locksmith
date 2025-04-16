3374567be55f3033ddf3fc5b84755a6abda54186
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-04-16 12:32:18 +0800

    add link tag for social platform

--- src/components/SocialItem.js
+++ src/components/SocialItem.js
@@ -1,14 +1,16 @@
 import Icon from "./Icon";
 
-export default function SocialItem({ icon, social, color }) {
+export default function SocialItem({ icon, social, color, link = "#" }) {
   return (
     <li className="flex flex-row items-center gap-3 w-60 lg:w-fit ">
-      <div
-        className={`flex items-center justify-center ${color} rounded-full w-15 h-15`}
-      >
-        <Icon icon={icon} className={"text-white w-5"} />
-      </div>
-      <p className="text-lg font-semibold lg:hidden">{social}</p>
+      <a href={link} target="_blank">
+        <div
+          className={`flex items-center justify-center ${color} rounded-full w-15 h-15`}
+        >
+          <Icon icon={icon} className={"text-white w-5"} />
+        </div>
+        <p className="text-lg font-semibold lg:hidden">{social}</p>
+      </a>
     </li>
   );
 }
